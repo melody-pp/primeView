5035ef4d4ee523b0d70001c80b6d6344d487c32d
皮娟 <trytry8090@163.com>
2017-11-30 01:42:50 +0800

    accordion

--- src/components/VueAccordion.vue
+++ src/components/VueAccordion.vue
@@ -1,56 +1,110 @@
 <template>
-  <div :class="accordionClass" :style="divStyles">
-    <ul>
+  <div class="vue-accordion">
+    <ul @mouseleave="mouseLeave()">
       <partialAccordion
         v-for="(item,index) in items"
         :item="item"
         :key="index"
-        :styles="styles"
+        @mouseenter.native="mouseEnter(index)"
       ></partialAccordion>
     </ul>
+    <div class="accordionModel">
+      <table>
+        <tbody>
+        <tr>
+          <td class="sp" :class="{active: hoverIndex === 0}"></td>
+          <td><</td>
+          <td>视</td>
+          <td>觉</td>
+          <td class="sp" :class="{active: hoverIndex === 1}"></td>
+          <td>表</td>
+          <td>现</td>
+          <td>></td>
+          <td class="sp" :class="{active: hoverIndex === 2}"></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script>
-  import { get } from 'lodash'
   import partialAccordion from './VueAccordionPartial.vue'
+  import index from '../router/index'
 
   export default {
     name: 'vue-accordion',
+
+    data () {
+      return {
+        hoverIndex: null,
+      }
+    },
+
     props: {
       items: Array,
-      styles: {
-        type: Object,
-        default () {
-          return {}
-        }
+    },
+
+    components: {partialAccordion},
+
+    methods: {
+      mouseEnter (index) {
+        this.hoverIndex = index
       },
-      accordionClass: {
-        type: String,
-        default: 'vue-accordion'
+
+      mouseLeave () {
+        this.hoverIndex = null
       }
-    },
-    computed: {
-      divStyles () {
-        return get(this, 'styles.div', {})
-      }
-    },
-    components: {partialAccordion}
+    }
   }
 </script>
 <style>
   .vue-accordion {
     width: 100%;
+    height: 500px;
     overflow: hidden;
-    height: 200px;
-    max-width: 900px;
+    position: relative;
   }
 
   .vue-accordion ul {
+    margin: 0;
+    padding: 0;
     width: 100%;
     height: 100%;
     display: table;
     table-layout: fixed;
-    margin: 0;
-    padding: 0;
   }
+
+  .accordionModel {
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: #fff;
+    opacity: 1;
+    width: 100%;
+    height: 100%;
+    font-size: 44px;
+    transition: all 500ms;
+  }
+
+  .accordionModel table {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+  }
+
+  .accordionModel table td {
+    padding: 0;
+    background: rgba(0, 0, 0, 0.4);
+  }
+
+  .accordionModel table td.sp {
+    width: 0;
+    transition: all 500ms;
+    background: rgba(0, 0, 0, 0);
+  }
+
+  .accordionModel table td.active {
+    width: 850px;
+  }
+
 </style>
--- src/pages/products/Products.vue
+++ src/pages/products/Products.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    products
+    <VueAccordion :items="items"></VueAccordion>
   </div>
 </template>
 
 <script>
+  import VueAccordion from '../../components/VueAccordion.vue'
+
   export default {
     data () {
-      return {}
-    }
+      return {
+        items: [
+          {image: require('../../assets/top_pic.jpg'),},
+          {image: require('../../assets/top_pic.jpg'),},
+          {image: require('../../assets/top_pic.jpg'),},
+        ]
+      }
+    },
+
+    components: {VueAccordion}
   }
 </script>
