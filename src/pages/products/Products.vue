d9b2f642d01a3050a23e773be1b0b2192ff228b3
皮娟 <trytry8090@163.com>
2017-12-03 15:53:37 +0800

    perspective effect

--- src/pages/products/Products.vue
+++ src/pages/products/Products.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="productsBox">
-    <div class="topPic">
+    <div class="topPic" ref="pic" :style="topPicStyle" @mousemove="mousemove" @mouseleave="mouseleave">
       <img src="../../assets/products_top.jpg" width="100%">
       <img src="../../assets/products_01.png" alt="">
       <img src="../../assets/products_02.png" alt="">
@@ -12,24 +12,52 @@
     <img src="../../assets/products_middle.jpg" width="100%">
     <img src="../../assets/products_bottom.jpg" width="100%">
   </div>
-
 </template>
 
 <script>
   export default {
     data () {
-      return {}
+      return {
+        topPicStyle: {
+          position: 'relative',
+          transition: 'transform .1s',
+          boxShadow: '0 0 15px rgba(0, 0, 0, .25)',
+          transform: ''
+        }
+      }
     },
+    methods: {
+      mousemove (event) {
+        const $pic = this.$refs.pic
+
+        const x = event.pageX - $pic.offsetLeft
+        const y = event.pageY - $pic.offsetTop
+        const centerX = $pic.offsetWidth / 2
+        const centerY = $pic.offsetHeight / 2
+
+        const deltaX = x - centerX
+        const deltaY = y - centerY
+        const percentX = deltaX / centerX
+        const percentY = deltaY / centerY
+
+        const deg = 10
+
+        this.topPicStyle.transform = `rotateX(${deg * -percentY}deg) rotateY(${deg * percentX}deg)`
+      },
+
+      mouseleave () {
+        this.topPicStyle.transform = ''
+      }
+    }
   }
 </script>
 
 <style scoped>
   .productsBox {
     padding-top: 65px;
-  }
-
-  .topPic {
     position: relative;
+    perspective: 3000px;
+
   }
 
   .topPic img:not(:nth-child(1)) {
