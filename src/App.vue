4b0a271f4968d403a7e207e771ce67f807c7d07b
皮娟 <trytry8090@163.com>
2017-12-11 17:10:58 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <keep-aliv>
-      <router-view></router-view>
-    </keep-aliv>
-
+    <router-view></router-view>
   </div>
 </template>
 
@@ -17,43 +14,34 @@
   @font-face {
     font-family: 'SourceHanSansCN-ExtraLight';
     src: url('./font/SourceHanSansCN-ExtraLight.otf');
-    font-weight: 100;
-    font-style: normal;
   }
 
   @font-face {
     font-family: 'SourceHanSansCN-Light';
     src: url('./font/SourceHanSansCN-Light.otf');
-    font-weight: 100;
-    font-style: normal;
   }
 
   @font-face {
     font-family: 'SourceHanSansCN-Regular';
     src: url('./font/SourceHanSansCN-Regular.otf');
-    font-weight: 100;
-    font-style: normal;
   }
 
   @font-face {
     font-family: 'SourceHanSansCN-Medium';
     src: url('./font/SourceHanSansCN-Medium.otf');
-    font-weight: 100;
-    font-style: normal;
+
   }
 
   @font-face {
     font-family: 'SourceHanSansCN-Normal';
     src: url('./font/SourceHanSansCN-Normal.otf');
-    font-weight: 100;
-    font-style: normal;
+
   }
 
   @font-face {
     font-family: 'SourceHanSansCN-Bold';
     src: url('./font/SourceHanSansCN-Bold.otf');
-    font-weight: 100;
-    font-style: normal;
+
   }
 
   /* @media only screen and (max-width: 1024px) {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import '@/lib'
-
+import './lib'
+import 'es6-promise/auto'
 Vue.config.productionTip = false
 
 new Vue({
