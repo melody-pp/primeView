85b39373067b73f13f3092eac05cdbab8557de99
王跃 <hust_wangyue@foxmail.com>
2017-12-15 19:17:01 +0800

    add debounce

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 export const sample = arr => arr[Math.floor(Math.random() * arr.length)]
+export const debounce = (func, wait = 100) => {
+  let timeout
 
-export default {sample}
+  return () => {
+    clearTimeout(timeout)
+    timeout = setTimeout(func, wait)
+  };
+};
+
+export default {sample, debounce}
