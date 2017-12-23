1ff30e44bd26bfdc2b19bd8964c7e811ed01f93a
wangyue <hust_wangyue@foxmail.com>
2017-12-23 18:05:06 +0800

    handle error

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    页面不存在
-    <router-link to="/">返回首页</router-link>
+    <main class="main">
+      <div class="icon">
+        404
+      </div>
+      <p class="desc">这个页面不存在哦 ╮(╯_╰)╭</p>
+      <p><a href="/">返回首页</a></p>
+    </main>
+    <footer class="footer">
+      <div class="copyRight">
+        © 2017 首页映像 &nbsp; 京 ICP 备 17001316 号 -1
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -11,6 +21,137 @@
   }
 </script>
 
-<style scoped>
+<style>
+  html,
+  body,
+  iframe,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  main,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
 
+  body {
+    text-align: center;
+  }
+
+  .main {
+    margin-top: 130px;
+  }
+
+  .main .icon {
+    width: 120px;
+    height: 120px;
+    border: 4px solid #5f7d8c;
+    border-radius: 50%;
+    margin: 0px auto;
+    position: relative;
+    line-height: 120px;
+    font-size: 30px;
+    color: #5f7d8c;
+  }
+
+  .main .icon .line {
+    width: 80px;
+    height: 4px;
+    background-color: #5f7d8c;
+    display: inline-block;
+    position: absolute;
+    top: 50%;
+    left: 20px;
+  }
+
+  .main .desc {
+    margin-top: 30px;
+    margin-bottom: 70px;
+    font-size: 18px;
+    line-height: 1.5em;
+    color: #a7a7a7;
+    padding: 0 20px;
+  }
+
+  .main a {
+    text-decoration: none;
+    font-size: 14px;
+    color: #545454;
+  }
+
+  .footer {
+    margin-top: 100px;
+    margin-bottom: 20px;
+    color: #AAACB4;
+    font-size: 13px;
+    font-weight: 100;
+  }
 </style>
